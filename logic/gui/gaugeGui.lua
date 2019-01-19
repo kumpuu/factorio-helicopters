eef7337ab75fb09ddc0aa92298d91d7f0ca1258a
kumpu <kumpuu@web.de>
2019-01-19 12:55:35 +0100

    Fixed that fuel beep mute button would disappear depending on gui scale. Updated changelog.

--- logic/gui/gaugeGui.lua
+++ logic/gui/gaugeGui.lua
@@ -294,17 +294,15 @@ gaugeGui =
 		els.gauge_fs = self:buildGauge(els.root, "gauge_fs", {"fuel", "speed"}, {"fuel"})
 		els.gauge_hr = self:buildGauge(els.root, "gauge_hr", {"height", "rpm"})
 
-			els.gauge_hr.speakerButtonFlow = els.gauge_hr.elem.add
-			{
-				type = "flow",
-				name = self.prefix .. "speakerButtonFlow",
-				direction = "horizontal",
-				tooltip = {"heli-gui-gauges-tt"},
-			}
-			els.gauge_hr.speakerButtonFlow.style.left_padding = 145
-			els.gauge_hr.speakerButtonFlow.style.top_padding = 150
+		els.gauge_hr.speakerButtonFlow = els.root.add
+		{
+			type = "flow",
+			name = self.prefix .. "speakerButtonFlow",
+			direction = "horizontal",
+			tooltip = {"heli-gui-gauges-tt"},
+		}
 
-			self:setMuted(Entity.get_data(self.player, "gaugeGui_muted"))
+		self:setMuted(Entity.get_data(self.player, "gaugeGui_muted"))
 
 		self:setGauge("gauge_fs", "fuel", self.pointerData.fuel.min)
 		self:setGauge("gauge_fs", "speed", self.pointerData.speed.min)
