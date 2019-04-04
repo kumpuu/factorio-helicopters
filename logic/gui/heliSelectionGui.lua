4da631900327d243a68bef5f81bb20bd5f44636d
DeltaNedas <39013340+DeltaNedas@users.noreply.github.com>
2019-04-04 17:07:25 +0100

    train error fix?

--- logic/gui/heliSelectionGui.lua
+++ logic/gui/heliSelectionGui.lua
@@ -178,7 +178,9 @@ heliSelectionGui =
 
 	updateCamPositions = function(self)
 		for k, curCam in pairs(self.guiElems.cams) do
-			curCam.cam.position = curCam.heli.baseEnt.position
+			if curCam.valid then
+				curCam.cam.position = curCam.heli.baseEnt.position
+			end
 		end
 	end,
 
